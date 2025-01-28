17be798f034d030309d652aa102f1c6901d09007
Nyzuro <marco.strs27@gmail.com>
2025-01-28 09:15:48 +0100

    feat(air09): finished air09 exercise

---
+++ air09.js
@@ -0,0 +1,26 @@
+const arrayRotate = (array) => {
+    const rotatedArray = []
+    const firstElement = array.shift()
+    rotatedArray.push(...array, firstElement)
+    return rotatedArray
+}
+
+const isValidArguments = (arguments) => {
+    if (arguments.length < 1) {
+        console.error("Le nombre d'arguments est trop faible")
+        process.exit()
+    }
+    return arguments
+}
+
+const getArguments = () => {
+    const arguments = process.argv.slice(2)
+    return arguments
+}
+
+function getRotatedArray() {
+    const array = isValidArguments(getArguments())
+    return arrayRotate(array)
+}
+
+console.log(getRotatedArray())
