1f7fff6cbc72d481d802ac23b85cd911c48f5ccc
Nyzuro <marco.strs27@gmail.com>
2025-01-31 17:06:10 +0100

    fix error handling

--- air00.js
+++ air00.js
@@ -1,48 +1,49 @@
 function split(stringToCut, stringSeparators) {
-    const strSplit = []
-    let string = ""
-    for (let i = 0; stringToCut[i]; i++) {
-        let isSeparator = false
-        for (const separator of stringSeparators) {
-            if (stringToCut[i] === separator) {
-                isSeparator = true
-                if (string) {
-                    strSplit.push(string)
-                    string = ""
-                }
-            }
+  const strSplit = [];
+  let string = "";
+  for (let i = 0; i < stringToCut.length; i++) {
+    let isSeparator = false;
+    for (const separator of stringSeparators) {
+      if (stringToCut[i] === separator) {
+        isSeparator = true;
+        if (string) {
+          strSplit.push(string);
+          string = "";
         }
-        if (isSeparator === false) {
-            string += stringToCut[i]
-        }
+      }
     }
-    strSplit.push(string)
-    return strSplit
+    if (isSeparator === false) {
+      string += stringToCut[i];
+    }
+  }
+  strSplit.push(string);
+  return strSplit;
 }
 
 function isValidArguments(arguments) {
-    if (arguments.length !== 1) {
-        console.error("Il faut 1 argument")
-        process.exit()
+  if (arguments.length !== 1) {
+    console.error("Il faut 1 argument");
+    return;
+  }
+  for (const argument of arguments) {
+    if (!isNaN(argument)) {
+      console.error("Entrez une chaine de caractere");
+      return;
     }
-    for (const argument of arguments) {
-        if (!isNaN(argument)) {
-            console.error("Entrez une chaine de caractere")
-            process.exit()
-        }
-    }
-    return arguments
+  }
+  return arguments;
 }
 
 function getArguments() {
-    const arguments = process.argv.slice(2)
-    return arguments
+  const arguments = process.argv.slice(2);
+  return arguments;
 }
 
 function getStringSplit() {
-    const strings = isValidArguments(getArguments())
-    const separators = [" ", "\n", "\t"]
-    return split(strings[0], separators)
+  const strings = isValidArguments(getArguments());
+  if (!strings) return;
+  const separators = [" ", "\n", "\t"];
+  return split(strings[0], separators);
 }
 
-console.log(getStringSplit())
+console.log(getStringSplit());
--- air01.js
+++ air01.js
@@ -1,50 +1,52 @@
 function split(stringToCut, stringSeparator) {
-    let string = ""
-    const strSplit = []
-    for (let index = 0; stringToCut[index]; index++) {
-        if (stringToCut[index] !== stringSeparator[0]) {
-            string += stringToCut[index]
-        } else {
-            let i = 0
-            let j = index
-            while (stringToCut[j] === stringSeparator[i]) {
-                i++
-                j++
-            }
-            if (i === stringSeparator.length && string) {
-                strSplit.push(string)
-                string = ""
-                index = j - 1
-            } else {
-                string += stringToCut[index]
-            }
-        }
+  let string = "";
+  const stringSplited = [];
+  for (let index = 0; index < stringToCut.length; index++) {
+    if (stringToCut[index] !== stringSeparator[0]) {
+      string += stringToCut[index];
+    } else {
+      let i = 0;
+      let j = index;
+      while (stringToCut[j] === stringSeparator[i]) {
+        i++;
+        j++;
+      }
+      if (i === stringSeparator.length && string) {
+        stringSplited.push(string);
+        string = "";
+        index = j - 1;
+      } else {
+        string += stringToCut[index];
+      }
     }
-    strSplit.push(string)
-    return strSplit
+  }
+  stringSplited.push(string);
+  return stringSplited;
 }
 
 function isValidArguments(arguments) {
-    if (arguments.length !== 2) {
-        console.error("Entrez une chaine de caractere et un separateur seulement")
-        process.exit()
-    } for (const argument of arguments) {
-        if (!isNaN(argument)) {
-            console.error("Entrez une chaine de caractere")
-            process.exit()
-        }
+  if (arguments.length !== 2) {
+    console.error("Entrez une chaine de caractere et un separateur seulement");
+    return;
+  }
+  for (const argument of arguments) {
+    if (!isNaN(argument)) {
+      console.error("Entrez une chaine de caractere");
+      return;
     }
-    return arguments
+  }
+  return arguments;
 }
 
 function getArguments() {
-    const arguments = process.argv.slice(2)
-    return arguments
+  const arguments = process.argv.slice(2);
+  return arguments;
 }
 
 function getStringSplit() {
-    const strings = isValidArguments(getArguments())
-    return split(strings[0], strings[1])
+  const strings = isValidArguments(getArguments());
+  if (!strings) return;
+  return split(strings[0], strings[1]);
 }
 
-console.log(getStringSplit())
+console.log(getStringSplit());
