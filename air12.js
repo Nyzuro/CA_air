6eb9a4c352e62bbef83c2d9a8a518bed88b3f2a1
Nyzuro <marco.strs27@gmail.com>
2025-02-06 14:34:01 +0100

    feat(air12): finished air12 exercise

--- air12.js
+++ air12.js
@@ -18,3 +18,47 @@ const partition = (array, low, high) => {
   swap(array, i + 1, high);
   return i + 1;
 };
+
+const quickSort = (array, low, high) => {
+  if (low < high) {
+    pivot = partition(array, low, high);
+
+    quickSort(array, low, pivot - 1);
+    quickSort(array, pivot + 1, high);
+  }
+  return array.join(" ");
+};
+
+const isValidArguments = (arguments) => {
+  if (arguments.length < 1) {
+    console.error("Le programme a besoin d'un argument pour fonctionner");
+    return;
+  }
+  return arguments;
+};
+
+const isValidNumber = (numbers) => {
+  for (let i = 0; i < numbers.length; i++) {
+    if (isNaN(numbers[i])) {
+      console.error("N'entrez que des nombres");
+      return;
+    }
+    numbers[i] = Number(numbers[i]);
+  }
+  return numbers;
+};
+
+const getArguments = () => {
+  const arguments = process.argv.slice(2);
+  return arguments;
+};
+
+const getArraySorted = () => {
+  const arguments = isValidArguments(getArguments());
+  if (!arguments) return;
+  const numbers = isValidNumber(arguments);
+  if (!numbers) return;
+  return quickSort(numbers, 0, numbers.length - 1);
+};
+
+console.log(getArraySorted());
