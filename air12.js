5b4288abec1273fda3788eee39479ef41ac4763f
Nyzuro <marco.strs27@gmail.com>
2025-02-06 10:40:50 +0100

    finish function partition

--- air12.js
+++ air12.js
@@ -2,8 +2,6 @@ const swap = (array, i, j) => {
   const temp = array[i];
   array[i] = array[j];
   array[j] = temp;
-
-  return array;
 };
 
 const partition = (array, low, high) => {
@@ -13,6 +11,10 @@ const partition = (array, low, high) => {
   for (let j = low; j < high; j++) {
     if (array[j] < pivot) {
       i++;
+      swap(array, i, j);
     }
   }
+
+  swap(array, i + 1, high);
+  return i + 1;
 };
